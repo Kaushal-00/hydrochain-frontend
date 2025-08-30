1e061218ce03447a0092a16d43cd1740cda7a5cb
rudraa19 <rudra19206@gmail.com>
2025-08-30 23:01:56 +0530

    chore: implement API based credit generator with dynamic data

--- src/Pages/PlantDashboard.jsx
+++ src/Pages/PlantDashboard.jsx
@@ -1,4 +1,5 @@
-import { client } from "../../config";
+import axios from "axios";
+import { client, NPOINT_URL } from "../../config";
 import CreditBalanceCard from "../components/CreditBalanceCard";
 import DataTable from "../components/DataTable";
 import DisplayCard from "../components/DisplayCard";
@@ -9,7 +10,28 @@ import { useNavigate } from "react-router-dom";
 const PlantDashboard = () => {
     const [username, setUsername] = useState("loading...");
     const [userData, setUserData] = useState();
+    const [plantDataIndex, setPlantDataIndex] = useState();
     const navigate = useNavigate();
+
+    const plantData = [
+        {
+            date: "2001-08-30",
+            hydrogenProduced: 1101,
+            purity: 70,
+            electricityConsumed: 78,
+            electrolyzerEfficiency: 5,
+            deviceId: "DEVICE-002"
+
+        },
+        {
+            date: "2025-08-30",
+            hydrogenProduced: 100,
+            purity: 99,
+            electricityConsumed: 500,
+            electrolyzerEfficiency: 75,
+            deviceId: "DEVICE-001"
+        }
+    ];
 
     useEffect(() => {
         const fetchUser = async () => {
@@ -21,12 +43,26 @@ const PlantDashboard = () => {
 
                 const temp = await client.get(`/users/${fetchedUsername}`);
                 setUserData(temp.data.data);
+
+                const index = await axios.get(NPOINT_URL);
+                setPlantDataIndex(parseInt(index?.data?.plant));
             } catch (err) {
                 console.log("Failed to fetch user: ", err);
             }
         }
         fetchUser();
     }, []);
+
+    async function issueRequest() {
+        try {
+            await client.post('/request/issues', {
+                amount: 1,
+                metadata: JSON.stringify(plantData[plantDataIndex])
+            });
+        } catch (err) {
+            console.error("Request failed:", err.response?.data || err.message);
+        }
+    }
 
     function creditHistroy(e) {
         return e.filter(item => item.type === "ISSUE");
@@ -67,15 +103,8 @@ const PlantDashboard = () => {
             <div className="flex justify-center">
                 <IoTDataFeedCard
                     requirements="date, hydrogenProduced, purity, electricityConsumed, electrolyzerEfficiency, deviceId"
-                    values={{
-                        date: "2025-08-30",
-                        hydrogenProduced: 100,
-                        purity: 99,
-                        electricityConsumed: 500,
-                        electrolyzerEfficiency: 75,
-                        deviceId: "DEVICE-001"
-                    }}
-                    onAction={() => alert("Credits Generated!")}
+                    values={plantData[plantDataIndex]}
+                    onAction={() => issueRequest()}
                     actionLabel="Generate Credits"
                 />
                 <CreditBalanceCard current={userData?.creditSummary?.activeAmount ?? '-'} pending={userData?.creditSummary?.pendingAmount ?? '-'} lifetime={userData?.creditSummary?.lifeTimeGeneratedCredits ?? '-'} />
